7e63f688d9db145f2a4dd417c754fb4ed6d2fc36
PRACHI DHINGRA <dhingra.prachi9@gmail.com>
2021-10-22 22:41:23 +0530

    update v1

--- index.js
+++ index.js
@@ -6,12 +6,87 @@ function getData() {
 	})
 }
 
-getData.getRepo = ((name) => {
+getData.getUserInfo = ((name) => {
     return new Promise((done, err) => {
         if(typeof name === 'undefined') {
             err('You forgot to provide User Name')
         } else {
             fetch(`https://api.github.com/users/${name}`)
+                .then(response => {
+                    if (response.status === 200) {
+                        return response.json()
+                    } else {
+                        err(`Sorry, Bad response code : ` + response.status)
+                    }
+                })
+                .then(json => done(json))
+                .catch(error => console.error(`Sorry, Error: ` + error))
+        }
+    })
+})
+
+getData.getRepos = ((name) => {
+    return new Promise((done, err) => {
+        if(typeof name === 'undefined') {
+            err('You forgot to provide User Name')
+        } else {
+            fetch(`https://api.github.com/users/${name}/repos`)
+                .then(response => {
+                    if (response.status === 200) {
+                        return response.json()
+                    } else {
+                        err(`Sorry, Bad response code : ` + response.status)
+                    }
+                })
+                .then(json => done(json))
+                .catch(error => console.error(`Sorry, Error: ` + error))
+        }
+    })
+})
+getData.getFollowers = ((name) => {
+    return new Promise((done, err) => {
+        if(typeof name === 'undefined') {
+            err('You forgot to provide User Name')
+        } else {
+            fetch(`https://api.github.com/users/${name}/followers`)
+                .then(response => {
+                    if (response.status === 200) {
+                        return response.json()
+                    } else {
+                        err(`Sorry, Bad response code : ` + response.status)
+                    }
+                })
+                .then(json => done(json))
+                .catch(error => console.error(`Sorry, Error: ` + error))
+        }
+    })
+})
+
+getData.getOrgDetail = ((name) => {
+    return new Promise((done, err) => {
+        if(typeof name === 'undefined') {
+            err('You forgot to provide User Name')
+        } else {
+            fetch(`https://api.github.com/orgs/${name}`)
+                .then(response => {
+                    if (response.status === 200) {
+                        return response.json()
+                    } else {
+                        err(`Sorry, Bad response code : ` + response.status)
+                    }
+                })
+                .then(json => done(json))
+                .catch(error => console.error(`Sorry, Error: ` + error))
+        }
+    })
+})
+
+getData.getOrgRepos = ((name) => {
+    return new Promise((done, err) => {
+        if(typeof name === 'undefined') {
+            err('You forgot to provide User Name')
+        } else {
+            fetch(`https://api.github.com/orgs/${name}/repos`)
                 .then(response => {
                     if (response.status === 200) {
                         return response.json()
--- npm_test.js
+++ npm_test.js
@@ -1,7 +1,34 @@
 import gitInfo from "./index.js";
 
-gitInfo.getRepo("prachidhingra09").then(obj => {  
+gitInfo.getUserInfo("prachidhingra09").then(obj => { 
+    console.log('------------------------USER INFO----------------------------------'); 
     console.log(obj)
 }).catch(err => {
     console.log(err)
 })
+gitInfo.getRepos("prachidhingra09").then(obj => {  
+    console.log('------------------------ALL REPOS----------------------------------');
+    console.log(obj)
+}).catch(err => {
+    console.log(err)
+})
+gitInfo.getFollowers("prachidhingra09").then(obj => {  
+    console.log('------------------------ALL FOLLOWERS-------------------------------');
+    console.log(obj)
+}).catch(err => {
+    console.log(err)
+})
+gitInfo.getOrgDetail("Tune-Zone").then(obj => {  
+    console.log('------------------------ORG DETAIL-------------------------------');
+    console.log(obj)
+}).catch(err => {
+    console.log(err)
+})
+
+gitInfo.getOrgRepos("Tune-Zone").then(obj => {  
+    console.log('------------------------ORG REPOS-------------------------------');
+    console.log(obj)
+}).catch(err => {
+    console.log(err)
+})
+
